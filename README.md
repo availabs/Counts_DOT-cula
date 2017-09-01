afbf4d0a9c6c3485e6e568a567c69bbdad0782e3
Paul Tomchik <pauljtomchik@live.com>
2017-09-01 16:50:18 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Counts DOT-cula
+
+``` graphql
+{
+  allDotShortCountSpeeds(first:10) {
+      edges {
+      node {
+        rg,
+        regionCode,
+        federalDirection
+      }
+    }
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const { join } = require('path');
+
+const express = require('express');
+const { postgraphql } = require('postgraphql');
+
+const { trafficDataCreds } = require('./config/postgres.creds');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+const options = {
+  disableDefaultMutations: true,
+  graphiql: true,
+  watchPg: true,
+  // showErrorStack: 'json',
+  extendedErrors: ['hint', 'detail', 'errcode'],
+  enableCors: true,
+  exportJsonSchemaPath: join(__dirname, 'schemas/postgraphql.json'),
+  exportGqlSchemaPath: join(__dirname, 'schemas/postgraphql.graphql'),
+};
+
+const trafficDataOptions = {
+  graphqlRoute: '/traffic_data/graphql',
+  graphiqlRoute: '/traffic_data/graphiql',
+  exportJsonSchemaPath: join(__dirname, 'schemas/postgraphql.trafficData.json'),
+  exportGqlSchemaPath: join(__dirname, 'schemas/postgraphql.trafficData.graphql'),
+};
+
+const trafficDataPostgraphqlMiddleware = postgraphql(trafficDataCreds, 'public', Object.assign({}, options, trafficDataOptions));
+
+app.use(trafficDataPostgraphqlMiddleware);
+
+process.stdout.write(`GraphQL Server started on port ${PORT}\n`);
+app.listen(PORT);
